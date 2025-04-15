f7010c65d53a9d8807c4cc95baeccdc300dbacb9
Vany <vsu205@nyu.edu>
2025-04-15 14:23:17 -0400

    pushing changes

---
+++ VSU205-S25-CodeLab1-Week4-MoreFileIO/Assets/Scripts/InputEntry.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class InputEntry
+{
+    public string playerName;
+    public int points;
+
+    public InputEntry(string name, int points)
+    {
+        playerName = name;
+        this.points = points;
+    }
+
+}
+
